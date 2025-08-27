1a3c14136f18af1c261871833fa9b2fdb28e7006
DeadlyDragon9631 <ayushk.ug23.me@nitp.ac.in>
2025-08-27 15:24:37 +0530

    LEC-45(Questions on LL)

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -2,38 +2,38 @@ package LinkedList;
 
 public class LL {
 
-    private Node head;
-    private Node tail;
+    private ListNode head;
+    private ListNode tail;
     private int size;
     LL(){
         this.size=0;
     }
 
-    private class Node{
-        private int value;
-        private Node next;
+    private class ListNode {
+        private int val;
+        private ListNode next;
 
-        public Node(int value) {
-            this.value = value;
+        public ListNode(int val) {
+            this.val = val;
         }
 
-        public Node(int value, Node next) {
-            this.value = value;
+        public ListNode(int val, ListNode next) {
+            this.val = val;
             this.next = next;
         }
     }
 
     public void insertFirst(int val){
-        Node node= new Node(val, head);
-        head=node;
-        if(tail==null) tail=node;
+        ListNode listNode = new ListNode(val, head);
+        head= listNode;
+        if(tail==null) tail= listNode;
         size++;
     }
 
     public void display(){
-        Node temp= head;
+        ListNode temp= head;
         while(temp!=null){
-            System.out.print("["+temp.value+"] -> ");
+            System.out.print("["+temp.val +"] -> ");
             temp=temp.next;
         }
         System.out.println("END");
@@ -42,9 +42,9 @@ public class LL {
     public void insertLast(int val){
         if(tail==null) insertFirst(val);
         else{
-            Node node= new Node(val);
-            tail.next=node;
-            tail=node;
+            ListNode listNode = new ListNode(val);
+            tail.next= listNode;
+            tail= listNode;
             size++;
         }
     }
@@ -57,13 +57,13 @@ public class LL {
         else if(in==0) insertFirst(val);
         else if(in==size) insertLast(val);
         else{
-            Node temp=head;
+            ListNode temp=head;
             while(in>1){
                 temp=temp.next;
                 in--;
             }
-            Node node= new Node(val, temp.next);
-            temp.next=node;
+            ListNode listNode = new ListNode(val, temp.next);
+            temp.next= listNode;
             size++;
         }
     }
@@ -73,19 +73,19 @@ public class LL {
             System.out.println("List already empty");
             return -1;
         }
-        int val= head.value;
+        int val= head.val;
         head=head.next;
         if(head==null) tail=null;
         size--;
         return val;
     }
 
-    public Node get(int in){
+    public ListNode get(int in){
         if(in>=size){
             System.out.println("Array index out of bound");
             return null;
         }
-        Node temp=head;
+        ListNode temp=head;
         for(int i=0; i<in; i++){
             temp=temp.next;
         }
@@ -94,8 +94,8 @@ public class LL {
 
     public int deleteLast(){
         if(size<=1) return deleteFirst();
-        Node secondLast=get(size-2);
-        int val=tail.value;
+        ListNode secondLast=get(size-2);
+        int val=tail.val;
         tail=secondLast;
         tail.next=null;
         size--;
@@ -109,19 +109,155 @@ public class LL {
         }
         if(in==0) return deleteFirst();
         if(in==size-1) return deleteLast();
-        Node prev=get(in-1);
-        int val=prev.next.value;
+        ListNode prev=get(in-1);
+        int val=prev.next.val;
         prev.next=prev.next.next;
         size--;
         return val;
     }
 
-    public Node find(int val){
-        Node temp=head;
+    public ListNode find(int val){
+        ListNode temp=head;
         while(temp!=null){
-            if(temp.value==val) return temp;
+            if(temp.val ==val) return temp;
             temp=temp.next;
         }
         return null;
     }
+
+    //insert using recursion
+    public void insertrec(int val, int in){
+        fxn_insertrec(val,in,head);
+    }
+    private void fxn_insertrec(int val, int in, ListNode curr){
+        if(in==0){
+            ListNode listNode =new ListNode(val);
+            listNode.next=head;
+            head= listNode;
+            size++;
+            return;
+        }
+        if(in==1){
+            ListNode listNode =new ListNode(val);
+            listNode.next=curr.next;
+            curr.next= listNode;
+            size++;
+            return;
+        }
+        fxn_insertrec(val,in-1,curr.next);
+    }
+
+    //insert using recsursion by K.K
+    public void insertrec2(int val, int in){
+        head=fxn_recKK(val,in,head);
+    }
+    private ListNode fxn_recKK(int val, int in, ListNode curr){
+        if(in==0){
+            ListNode listNode = new ListNode(val,curr);
+            size++;
+            return listNode;
+        }
+        curr.next=fxn_recKK(val,in-1,curr.next);
+        return curr;
+    }
+
+    //Reversing a LL via recursion
+    public void reverseRec(){
+        fxnreverseRec(head);
+    }
+    private void fxnreverseRec(ListNode listNode){
+        if(listNode ==tail){
+            head=tail;
+            return;
+        }
+        fxnreverseRec(listNode.next);
+        tail.next= listNode;
+        tail= listNode;
+        tail.next=null;
+    }
+
+    //Reversing via iteration
+    public void reverseItr(){
+        if(size==0 || size==1) return;
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next= head.next;
+        while(curr!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+            if(next!=null) next=next.next;
+        }
+        //swapping head and tail
+        tail=head;
+        head=prev;
+    }
+
+    //Alternate k reversal
+    public ListNode reverseKGroup( int k) {
+         ListNode head=this.head;
+        boolean turn=true;
+        ListNode temp=head;
+        int left=1;
+        int right=1;
+        while(temp!=null){
+            while(temp!=null && right<left+k-1){
+                right++;
+                temp=temp.next;
+            }
+            if(temp==null) return head;
+            temp=temp.next;
+            if(turn) head= reverseBetween(head, left, right);
+            left=right+1;
+            right=left;
+            turn=!turn;
+        }
+        return head;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right) return head;
+        ListNode LEFT=null;
+        ListNode RIGHT=null;
+        ListNode prev=null;
+        ListNode curr=null;
+        ListNode next=null;
+        //finding initial values of all parameters
+        ListNode temp=head;
+        while(temp!=null){
+            if(left==2) LEFT= temp;
+            if(left==1){
+                curr=temp;
+            }
+            if(right==1){
+                RIGHT=temp.next;
+                temp.next=null;
+                break;
+            }
+            temp=temp.next;
+            right--;
+            left--;
+        }
+        next=curr.next;
+
+        //reversing given LL
+        while(curr!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+            if(next!=null) next=next.next;
+        }
+
+        //linking back together
+        if(LEFT!=null){
+            LEFT.next.next=RIGHT;
+            LEFT.next=prev;
+            return head;
+        }
+        else{
+            head.next=RIGHT;
+            return prev;
+        }
+    }
+
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,14 +1,29 @@
 package LinkedList;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-//        LL list= new LL();
+        LL list= new LL();
 //        list.insertFirst(3);
 //        list.insertFirst(4);
 //        list.insertFirst(5);
 //        list.insertLast(18);
 //        list.insertAt(15,3);
 //        list.insertAt(56,2);
+//        //list.display();
+//        list.insertrec(69,2);
+//        //list.display();
+//        list.insertrec(44,0);
+//        //list.display();
+//        list.insertrec2(61,2);
+//        //list.display();
+//        list.insertrec2(72,0);
+//        list.display();
+//        list.reverseRec();
+//        list.display();
+//        list.reverseItr();
+//        list.display();
 //        list.display();
 //        System.out.println(list.deleteFirst());
 //        list.display();
@@ -27,13 +42,21 @@ public class Main {
 //        list.display();
 //        list.displayReverse();
 
-        CircularLL list= new CircularLL();
-        list.insert(3);
-        list.insert(4);
-        list.insert(5);
-        list.insert(55);
+//        CircularLL list= new CircularLL();
+//        list.insert(3);
+//        list.insert(4);
+//        list.insert(5);
+//        list.insert(55);
+//        list.display();
+//        list.delete(5);
+//        list.display();
+
+        //Alternate k reversal
+        for(int i=1; i<10; i++){
+            list.insertLast(i);
+        }
         list.display();
-        list.delete(5);
+        list.reverseKGroup(2);
         list.display();
     }
 }
